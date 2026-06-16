109c7437daee7330183e47b17cf7c41832a27dcb
Dakitidunno <207196558+juanitoisback6@users.noreply.github.com>
2026-06-15 21:27:28 -0500

    Update lc.js

--- lc.js
+++ lc.js
@@ -1,11 +1,6 @@
 let l1 = [9,9,9,9,9,9,9]; 
 let l2 = [9,9,9,9];
-
- // [ 2, 4, 6, 5 ]
-//  [ 3, 4, 2 ]
-  
-  
-
+ 
 function addTwoNumbers (l1, l2) {
   
   let sumSav= 0;
@@ -16,21 +11,21 @@ function addTwoNumbers (l1, l2) {
      [l1,l2] = [l2,l1]; 
   }
   l1.reverse();
-  l2.reverse();
+   l2.reverse();
   
   for(let i=0; i<l1.length; i++){
-    console.log(i);
-    console.log(i+" sumsav "+ sumSav);
+  
     
     if( l2[i] != undefined){
-      console.log(i + " linea " + l2[i]);
+ 
        //Este if para saber si es menor a 10
-    if(l1[i]+l2[i]<10){
+    if(l1[i]+l2[i]+sumSav<10){
       output.push(l1[i]+l2[i]+ sumSav );
-      console.log(i+"sumsav"+ sumSav);
+      
       sumSav=0;
     }else{
-      let sisas = l1[i]+l2[i];
+      let sisas = l1[i]+l2[i]+sumSav;
+      sumSav=0;
       sisas = sisas.toString();
       
       sumSav = Number(sisas[0]);
@@ -39,48 +34,27 @@ function addTwoNumbers (l1, l2) {
        
     }
     } else{
-      
-      output.push(l1[i]+ sumSav );
+      let noks = l1[i]+sumSav;
+      sumSav=0;
+      noks = noks.toString();
+      if(noks.length > 1){
+         sumSav = Number(noks[0]);
+      let cambio = Number(noks[1]);
+      output.push(cambio );
+      }else{
+       
+      let cambio = Number(noks[0]);
+      output.push(cambio );
+      }
+     
     }
-   
-  
-    
   }
-   
-  // output = output.reverse();
-  // output= Number(output.join(""));
+   if(sumSav>0){
+    output.push(sumSav);
+  }
+    
    return output 
 };
 
 console.log(addTwoNumbers (l1, l2))
  
-
-
- // let noks = l1[i] + sumSav;
- //       noks= noks.toString();
- //      console.log("este es noks "+ noks);
- //      sumSav =  Number(noks[0]);
- //      console.log(sumSav)
- //      let noksAdd = Number(noks[1]);
- //      console.log(noksAdd)
- //      output.push(noksAdd);
-
-
-
-
-
-
- 
-  //    if(sumaSav>0){
-   //     l1[i]
-   //      sumaSav=0;
-   //      }
-
-
-
-// arra = [];
-  // for(let i = 0; i< l1.length; i++){
-  //  arra.push(l1[i]+l2[i]);
-   
-  // }
-  // return arra
