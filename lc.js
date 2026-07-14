7da3f97c6c8fbec512a7c7d9a7b5f52255fe07bd
Dakitidunno <207196558+juanitoisback6@users.noreply.github.com>
2026-07-13 22:53:58 -0500

    Refactor addTwoNumbers function with comments
    Refactor addTwoNumbers function for clarity and maintainability. Added comments for better understanding of the logic and flow.

--- lc.js
+++ lc.js
@@ -1,8 +1,13 @@
+
+ 
 function addTwoNumbers (li1, li2) {
-   
+
   
+  //arrays to save the data collected from linked lists
   let l1 = [];
   let l2 = [];
+  
+  //loop to collect data from each linked list and push inside the arrays l1 and l2
   
   while(li1 != null){
     l1.push(li1.val)
@@ -13,41 +18,55 @@ function addTwoNumbers (li1, li2) {
     li2= li2.next
   }
 
-  
+  // dos variables, una para guardar la unidad de reserva o carry number
   let sumSav= 0;
+  
+  // array to save the total sum of each iteratiion
   let output=[];
   
+  
+  //primero necesitamos saber que lista es más larga para luego sumarlas 
   if(l1.length < l2.length){
     
      [l1,l2] = [l2,l1]; 
   }
+  
+  // once we have sorted the long and short array, we reverse them
   l1.reverse();
    l2.reverse();
   
-  for(let i=0; i<l1.length; i++){
   
+  //loop to iterate l1 array
+  for(let i=0; i<l1.length; i++){
     
+    //this to verify if the iteration is not in an empty part of the array
     if( l2[i] != undefined){
  
-       //Este if para saber si es menor a 10
+      // conditional to verify if the sum gives us less than 10, it'll sum the numbers of array l1, array l2 and the sumSav that us the carry number
+      
     if(l1[i]+l2[i]+sumSav<10){
       output.push(l1[i]+l2[i]+ sumSav );
       
       sumSav=0;
-    }else{
+    }
+      // else that helps us when the result of the sum gives us more than 9, here we need to save the tens (if we sum 9+1 the 1 will be saved and the 0 displayed and saved in the output array) 
+      else{
       let sisas = l1[i]+l2[i]+sumSav;
       sumSav=0;
       sisas = sisas.toString();
-      
       sumSav = Number(sisas[0]);
       let addTo = Number(sisas[1]);
       output.push(addTo );
        
     }
-    } else{
+    } 
+    //else to know if the l2 has no number in the same position than the l1
+    else{
       let noks = l1[i]+sumSav;
       sumSav=0;
       noks = noks.toString();
+      
+      //if to know if the sum result has tens to save in sumSav
       if(noks.length > 1){
          sumSav = Number(noks[0]);
       let cambio = Number(noks[1]);
@@ -60,10 +79,17 @@ function addTwoNumbers (li1, li2) {
      
     }
   }
+  
+  //Aca se acaba la iteracion del primer loop
+  
+  
+  //here we check if there is a number at saved at the end of the iteration to push it into the array output
    if(sumSav>0){
     output.push(sumSav);
   }
   
+  
+  // class to create the linked list that is going to be outputted
 function ListNode(val, next) {
 
 this.val = (val===undefined ? 0 : val);
@@ -72,13 +98,17 @@ this.next = (next===undefined ? null : next);
 } 
   
   let dummyNode = new ListNode(0);
-  let nodoAc= dummyNode;
+  var nodoAc = dummyNode;
   
   for(let i=0; i < output.length;i++){
     
     nodoAc.next = new ListNode(output[i]);
-    nodoAc= nodoAc.next;
+    nodoAc = nodoAc.next;
   }
     
    return dummyNode.next 
+
 };
+
+console.log(addTwoNumbers (l1, l2))
+ 
